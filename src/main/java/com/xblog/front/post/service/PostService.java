f6e611416757dd78fcf3ff6283c34426ff97bdaa
badgelatte <saucy5939@gmail.com>
2024-08-29 19:34:04 +0900

    feat: 게시물 관리하는 Service
    특정 게시물 조회, 카테고리에 해당하는 모든 게시물 조회, 게시물 생성, 게시물 수정, 게시물 삭제 method로 구성됨

---
+++ src/main/java/com/xblog/front/post/service/PostService.java
@@ -0,0 +1,20 @@
+package com.xblog.front.post.service;
+
+import com.xblog.front.post.dto.AddPostDto;
+import com.xblog.front.post.dto.GetPostDto;
+import com.xblog.front.post.dto.ModifyPostRequest;
+import com.xblog.front.post.dto.ModifyPostResponse;
+
+import java.util.List;
+
+public interface PostService {
+    List<GetPostDto> getPostList(Long partyId);
+
+    GetPostDto getPost(Long postId);
+    
+    AddPostDto addPost(AddPostDto addPostDto);
+
+    ModifyPostResponse modifyPost(ModifyPostRequest request, Long postId);
+
+    void deletePost(Long postId);
+}
---
+++ src/main/java/com/xblog/front/post/service/impl/PostServiceImpl.java
@@ -0,0 +1,43 @@
+package com.xblog.front.post.service.impl;
+
+import com.xblog.front.post.dto.AddPostDto;
+import com.xblog.front.post.dto.GetPostDto;
+import com.xblog.front.post.adaptor.PostAdaptor;
+import com.xblog.front.post.dto.ModifyPostRequest;
+import com.xblog.front.post.dto.ModifyPostResponse;
+import com.xblog.front.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    private final PostAdaptor postAdaptor;
+
+    @Override
+    public List<GetPostDto> getPostList(Long partyId) {
+        return postAdaptor.getPostList(partyId);
+    }
+
+    @Override
+    public GetPostDto getPost(Long postId) {
+        return postAdaptor.getPost(postId);
+    }
+
+    @Override
+    public AddPostDto addPost(AddPostDto addPostDto) {
+        return postAdaptor.createPost(addPostDto);
+    }
+
+    @Override
+    public ModifyPostResponse modifyPost(ModifyPostRequest request, Long postId) {
+        return postAdaptor.modifyPost(request, postId);
+    }
+
+    @Override
+    public void deletePost(Long postId) {
+        postAdaptor.deletePost(postId);
+    }
+}
