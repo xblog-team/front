28e96d6271d7c1a4b7971d6f425e9e34ad10c203
badgelatte <saucy5939@gmail.com>
2024-09-05 16:42:27 +0900

    feat: CategoryAdaptor 작성
    그룹 아이디를 가진 모든 카테고리를 조회하는 메소드

---
+++ src/main/java/com/xblog/front/category/adaptor/CategoryAdaptor.java
@@ -0,0 +1,9 @@
+package com.xblog.front.category.adaptor;
+
+import com.xblog.front.category.dto.GetCategoryDto;
+
+import java.util.List;
+
+public interface CategoryAdaptor {
+    List<GetCategoryDto> getCategories(Long partyId);
+}
---
+++ src/main/java/com/xblog/front/category/adaptor/impl/CategoryAdaptorImpl.java
@@ -0,0 +1,41 @@
+package com.xblog.front.category.adaptor.impl;
+
+import com.xblog.front.category.adaptor.CategoryAdaptor;
+import com.xblog.front.category.dto.GetCategoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoryAdaptorImpl implements CategoryAdaptor {
+    private final RestTemplate restTemplate;
+
+    @Value("http://localhost:8090")
+    String gatewayDomain;
+
+    public HttpHeaders makeHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+
+    @Override
+    public List<GetCategoryDto> getCategories(Long partyId) {
+        HttpHeaders headers = makeHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<List<GetCategoryDto>> exchange = restTemplate.exchange(
+                gatewayDomain + "/api/categories/category-parties/" + partyId,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<>() {
+                });
+        return exchange.getBody();
+    }
+}
