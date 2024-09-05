4756f76a18d007544996b6c6f05706f4af16b77c
badgelatte <saucy5939@gmail.com>
2024-09-05 16:51:17 +0900

    fix: @Setter, @NoArgsConstructor 추가, CategoryId를 categoryId로 수정
    Type Define Error의 해결을 위해 @Setter, @NoArgsConstructor을 추가
    다른 필드와의 통일성을 위해 CategoryId에서 맨 앞글자를 소문자로 수정

--- src/main/java/com/xblog/front/post/dto/GetPostDto.java
+++ src/main/java/com/xblog/front/post/dto/GetPostDto.java
@@ -2,13 +2,17 @@ package com.xblog.front.post.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class GetPostDto {
     private Long postId;
     private String title;
     private String content;
-    private Long CategoryId;
+    private Long categoryId;
     private String userId;
 }
