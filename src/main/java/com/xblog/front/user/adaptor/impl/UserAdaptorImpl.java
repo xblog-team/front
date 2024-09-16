902cfe6eae5c320e8f11a25150f60c9df07670b5
SeinJs <drasein@gmail.com>
2024-09-16 20:37:54 +0900

    Refactor: nickname 변경 adaptor 구현 완료

--- src/main/java/com/xblog/front/user/adaptor/impl/UserAdaptorImpl.java
+++ src/main/java/com/xblog/front/user/adaptor/impl/UserAdaptorImpl.java
@@ -1,5 +1,12 @@
 package com.xblog.front.user.adaptor.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,10 +17,31 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class UserAdaptorImpl implements UserAdaptor{
+    private final String USER_URL = "/api/user";
     private final RestTemplate restTemplate;
+
+    @Value("${gateway.api.url}")
+    private String gatewayDomain;
+
+    public HttpHeaders makeHeaders(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return httpHeaders;
+    }
 
     @Override
     public void changeNickname(String userId, String newNickname){
+        HttpHeaders headers = makeHeaders();
+        headers.set("X-USER-ID", userId);
+        HttpEntity<Void> httpEntity = new HttpEntity<>(headers);
+
+        restTemplate.exchange(
+            gatewayDomain + USER_URL + "/change-nickname?newNickname=" + newNickname, 
+            HttpMethod.PUT,
+            httpEntity,
+            Void.class
+        );
         
     }
 }
